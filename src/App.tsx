e9487aa0f78f7ac5d22f6843ad188e652b70a08a
lkasvr <lkasvr@outlook.com>
2024-02-08 19:53:27 -0300

    Storage & Database Design with appwrite

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { Route, Routes } from 'react-router-dom';
+import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Toaster } from './components/ui/toaster';
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* PUBLIC ROUTES */}
+        <Route element={<AuthLayout />}>
+          <Route
+            path="/sign-in"
+            element={<SigninForm />}
+          />
+          <Route
+            path="/sign-up"
+            element={<SignupForm />}
+          />
+        </Route>
+        {/* PRIVATE ROUTES */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+
+      <Toaster />
+    </main>
+  )
+}
+
+export default App;
