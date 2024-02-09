d1316bda25f81fa9febae8b4e2bfb1fefdcc65cc
lkasvr <lkasvr@outlook.com>
2024-02-09 06:41:55 -0300

    TanStack Query and Context providers implemented

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,13 +14,20 @@ import { Button } from "@/components/ui/button"
 import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
-import { Link } from "react-router-dom";
-import { createUserAccount } from "@/lib/appwrite/api";
+import { Link, useNavigate } from "react-router-dom";
 import { useToast } from "@/components/ui/use-toast";
+import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
 
 const SignupForm = () => {
   const { toast } = useToast();
-  const isLoading = false;
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const navigate = useNavigate();
+
+  const { mutateAsync: createUserAccount, isPending: isCreatingAccount } = useCreateUserAccount();
+
+  const { mutateAsync: signInAccount, isPending: isSignIn } = useSignInAccount();
+
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -42,7 +49,23 @@ const SignupForm = () => {
         title: 'Sign up failed. Please try again.'
       });
 
-    // const session = await signInAccount();
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password
+    });
+
+    if (!session)
+      return toast({ title: 'Sign in failed. Please try again' });
+
+    const isLoggedIn = await checkAuthUser();
+
+    if (isLoggedIn) {
+      form.reset();
+
+      navigate('/');
+    } else {
+      return toast({ title: 'Sign up failed. Please try again.' });
+    }
   }
 
   return (
@@ -134,7 +157,7 @@ const SignupForm = () => {
             type="submit"
             className="shad-button_primary"
           >
-            {isLoading ? (
+            {isCreatingAccount ? (
               <div className="flex-center gap-2">
                 <Loader />
                 Loading...
@@ -157,4 +180,4 @@ const SignupForm = () => {
   )
 }
 
-export default SignupForm
+export default SignupForm;
